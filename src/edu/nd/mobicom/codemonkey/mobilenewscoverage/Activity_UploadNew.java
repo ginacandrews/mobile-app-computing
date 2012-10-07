72de32f8b91624ee2c3b42d932868c69165ce408
ginaaandrews@gmail.com <ginaaandrews@gmail.com@1693de46-edb5-3723-b492-2aef37759891>
2012-10-07 23:38:06 +0000

    2012-10-07

---
+++ src/edu/nd/mobicom/codemonkey/mobilenewscoverage/Activity_UploadNew.java
@@ -0,0 +1,18 @@
+package edu.nd.mobicom.codemonkey.mobilenewscoverage;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_UploadNew extends Activity_Main {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadnew);
+    }
+
+}
