e45bb5d98de86e93cd8aa7417e0a24a5d4ac402d
ginaaandrews@gmail.com <ginaaandrews@gmail.com@1693de46-edb5-3723-b492-2aef37759891>
2012-10-27 21:08:07 +0000

    Can't make directory :(

--- src/edu/nd/mobicom/codemonkey/mobilenewscoverage/AlbumStorageDirFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.nd.mobicom.codemonkey.mobilenewscoverage;
-
-import java.io.File;
-
-abstract class AlbumStorageDirFactory {
-	public abstract File getAlbumStorageDir(String albumName);
-}
