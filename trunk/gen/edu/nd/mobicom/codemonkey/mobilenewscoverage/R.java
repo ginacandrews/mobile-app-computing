d3aa82794ad795ec549d894f3474f5a0b29ba5e2
ginaaandrews@gmail.com <ginaaandrews@gmail.com@1693de46-edb5-3723-b492-2aef37759891>
2012-10-28 19:05:51 +0000

    *Takes photos and videos * Signup with parse

--- trunk/gen/edu/nd/mobicom/codemonkey/mobilenewscoverage/R.java
+++ trunk/gen/edu/nd/mobicom/codemonkey/mobilenewscoverage/R.java
@@ -25,18 +25,16 @@ public final class R {
         public static final int TextView01=0x7f07000a;
         public static final int button1=0x7f070016;
         public static final int button2=0x7f070017;
-        public static final int checkBox1=0x7f070019;
-        public static final int compsignup_btn=0x7f07001b;
-        public static final int editText1=0x7f07000f;
-        public static final int editText2=0x7f070010;
-        public static final int editText3=0x7f070012;
-        public static final int imageView1=0x7f07001c;
+        public static final int compsignup_btn=0x7f07001c;
+        public static final int email=0x7f070012;
+        public static final int imageView1=0x7f07001d;
         public static final int main_btn=0x7f07000e;
-        public static final int menu_settings=0x7f07001e;
+        public static final int menu_settings=0x7f07001f;
         public static final int mymessages_btn=0x7f07000d;
         public static final int myuploads_btn=0x7f07000c;
+        public static final int password=0x7f070010;
         public static final int photo_btn=0x7f070006;
-        public static final int signup_btn=0x7f07001d;
+        public static final int signup_btn=0x7f07001e;
         public static final int tableRow1=0x7f070000;
         public static final int tableRow2=0x7f070002;
         public static final int tableRow3=0x7f070005;
@@ -46,12 +44,15 @@ public final class R {
         public static final int tableRow7=0x7f070015;
         public static final int tableRow8=0x7f070018;
         public static final int tableRow9=0x7f07001a;
+        public static final int terms_btn=0x7f07001b;
+        public static final int terms_checkbox=0x7f070019;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070007;
         public static final int textView3=0x7f070004;
         public static final int textView4=0x7f070011;
         public static final int textView5=0x7f070014;
         public static final int upload_btn=0x7f070003;
+        public static final int username=0x7f07000f;
         public static final int video_btn=0x7f070009;
     }
     public static final class layout {
